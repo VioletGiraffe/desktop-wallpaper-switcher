c7ff1753dfa8eaf88a9811b056c3178a2fe56eef
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2015-06-11 11:23:10 +0300

    Build script improvement

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -1,9 +1,9 @@
-set QTDIR=k:\Qt\5\5.4\msvc2013_opengl\
-
 RMDIR  /S /Q binaries\
 
+call set_qt_paths.bat
+
 pushd ..\
-%QTDIR%\bin\qmake.exe -tp vc -r
+%QTDIR32%\bin\qmake.exe -tp vc -r
 popd
 
 call "%VS120COMNTOOLS%VsDevCmd.bat"
@@ -11,18 +11,18 @@ msbuild ..\WallpaperSwitcher.sln /t:Build /p:Configuration=Release;PlatformTools
 
 xcopy /R /Y ..\bin\WallpaperSwitcher.exe binaries\
 
-xcopy /R /Y %QTDIR%\bin\Qt5Core.dll binaries\Qt\
-xcopy /R /Y %QTDIR%\bin\Qt5Gui.dll binaries\Qt\
-xcopy /R /Y %QTDIR%\bin\Qt5Widgets.dll binaries\Qt\
+xcopy /R /Y %QTDIR32%\bin\Qt5Core.dll binaries\Qt\
+xcopy /R /Y %QTDIR32%\bin\Qt5Gui.dll binaries\Qt\
+xcopy /R /Y %QTDIR32%\bin\Qt5Widgets.dll binaries\Qt\
 
-xcopy /R /Y %QTDIR%\bin\icu*.dll binaries\Qt\
+xcopy /R /Y %QTDIR32%\bin\icu*.dll binaries\Qt\
 
-xcopy /R /Y %QTDIR%\plugins\imageformats\qgif.dll binaries\Qt\imageformats\
-xcopy /R /Y %QTDIR%\plugins\imageformats\qico.dll binaries\Qt\imageformats\
-xcopy /R /Y %QTDIR%\plugins\imageformats\qjpeg.dll binaries\Qt\imageformats\
-xcopy /R /Y %QTDIR%\plugins\imageformats\qtiff.dll binaries\Qt\imageformats\
+xcopy /R /Y %QTDIR32%\plugins\imageformats\qgif.dll binaries\Qt\imageformats\
+xcopy /R /Y %QTDIR32%\plugins\imageformats\qico.dll binaries\Qt\imageformats\
+xcopy /R /Y %QTDIR32%\plugins\imageformats\qjpeg.dll binaries\Qt\imageformats\
+xcopy /R /Y %QTDIR32%\plugins\imageformats\qtiff.dll binaries\Qt\imageformats\
 
-xcopy /R /Y %QTDIR%\plugins\platforms\qwindows.dll binaries\Qt\platforms\
+xcopy /R /Y %QTDIR32%\plugins\platforms\qwindows.dll binaries\Qt\platforms\
 
 xcopy /R /Y %SystemRoot%\SysWOW64\msvcr120.dll binaries\msvcr\
 xcopy /R /Y %SystemRoot%\SysWOW64\msvcp120.dll binaries\msvcr\
---
+++ installer/set_qt_paths.bat
@@ -0,0 +1,2 @@
+set QTDIR32=c:\Development\Qt\5.4\msvc2013_opengl\
+set QTDIR64=c:\Development\Qt\5.4\msvc2013_64_opengl\
